6c52cfeb56c04a660bac51fda9cf1a3ed3b51fcd
wisecg <wisecg.neontetra@gmail.com>
2018-12-20 18:59:49 -0800

    start autoproc code

--- auto_process.py
+++ auto_process.py
@@ -1,19 +1,95 @@
 #!/usr/bin/env python3
-import sys, os, glob, datetime
+import sys, os, glob, datetime, json
+from pprint import pprint
+import numpy as np
+import pandas as pd
+from shutil import move
 
-def main():
 
-    # change these to the working coherent ones
-    data_path = "/Users/mjcenpa/Data/C1"
-    workdir = "/Users/mjcenpa/Data/C1/Processed/built_runs"
-    cwd = os.getcwd()
-    # os.chdir(workdir)
+def main(argv):
+    """
+    Automatic processing of COHERENT NaI(Tl) crystal data.
+    Uses "runDB.json" as a database.  When a crystal is scanned,
+    we input its serial number and run numbers, and run this code
+    to process the data and do file handling (moving)
+    Usage:
+    $ python auto_process.py [options]
+        default (no arguments): process all runs on this computer
+        -crys [serial no]: process data from a particular crystal
+        -sync : upload data to cenpa-rocks and delete the raw ORCA files
+    """
 
-    print("hi clint")
-    print("data_path:",data_path)
-    print("workdir:",workdir)
+    # -- load our run database and make it global --
+    global runDB
+    with open("runDB.json") as f:
+        runDB = json.load(f)
+
+    # -- parse command line args --
+    for i, opt in enumerate(argv):
+
+        if opt=="-crys":
+            sn = argv[i+1]
+            process_crystal(sn)
+            exit()
+
+    sns = [id for id in runDB.keys() if "UW" in id]
+    print(sns)
+
+
+
+
+def process_crystal(sn):
+    print(sn)
+
+
+def sandbox():
+    """ deleteme! """
+
+    with open("runDB.json") as f:
+        runDB = json.load(f)
+
+    runs_to_process = runDB["UW3"]["voltage"]
+
+    raw_files = list(set(glob.glob("{}/**/**/Data/*Run*".format(raw_path), recursive=True)))
+
+    for f in raw_files:
+        run = int(f.split('Run')[-1])
+        if run in runs_to_process:
+
+            mod_path = "/Users/ccenpa/Desktop/coherent/ORCA\ Experiments"
+
+            tmp = f.split("/")
+            fname = tmp[-1]
+            mon = tmp[-3]
+            yr = tmp[-4]
+
+            newfile = "{}/{}/{}/Data/{}".format(mod_path,yr,mon,fname)
+
+            # print(newfile)
+
+            cmd = "./getSpectrum {}".format(newfile)
+            sh(cmd)
+
+            # out_file = "NaI_ET_run{}.root".format(run)
+            # os.mkdir("{}/UW3".format(built_path))
+            # os.mkdir("{}/UW3/voltage/".format(built_path))
+            # os.mkdir("{}/UW3/voltage/600V".format(built_path))
+            # move(out_file, "{}/UW3/voltage/600V/{}".format(built_path, out_file))
+
+            # nope, do this on rocks
+            # sh("gzip {}".format(f))
+
+
+            exit()
 
     exit()
+
+    # change these to the working coherent ones
+    # data_path = "/Users/mjcenpa/Data/C1"
+    # workdir = "/Users/mjcenpa/Data/C1/Processed/built_runs"
+    # cwd = os.getcwd()
+    # os.chdir(workdir)
+
 
     # note: if this list ever gets too huge, you can switch to using os.walk
     raw_files = list(set(glob.glob("{}/**/**/Data/*Run*".format(data_path), recursive=True)))
@@ -47,4 +123,4 @@ def sh(cmd, sh=False):
 
 
 if __name__=="__main__":
-    main()
+    main(sys.argv[1:])
